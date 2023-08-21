9d1470efdd73c5432af1dd8c03dab7217ff5fd7b
mandy0207 <mandyatwork24@gmail.com>
2023-08-21 11:38:35 -0400

    Propeties Utility

---
+++ src/main/java/com/obsqura/utilities/TestProperties.java
@@ -0,0 +1,19 @@
+package com.obsqura.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+
+	
+	public Properties GetProperties() throws IOException {
+		
+		Properties prop = new Properties();
+		
+		FileInputStream fis = new FileInputStream("C:\\Sask\\Code\\workspace\\ObsquraTestAutomationFramework\\src\\main\\resources\\Configurations\\Config.properties");
+		prop.load(fis);
+		return prop;
+	}
+}
