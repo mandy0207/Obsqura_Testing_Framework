a0fa117816500897b18e50063cd6976beb639232
mandy0207 <mandyatwork24@gmail.com>
2023-08-23 12:11:29 -0400

    new test

--- src/test/java/com/obsqura/tests/BaseTest.java
+++ src/test/java/com/obsqura/tests/BaseTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import com.obsqura.Exception.NoBrowserFoundException;
@@ -68,9 +69,9 @@ public class BaseTest {
 	
 
 	
-//	@AfterClass
-//	public void TearDown() {
-//
-//		driver.quit();
-//	}
+	@AfterClass
+	public void TearDown() {
+
+		driver.quit();
+	}
 }
